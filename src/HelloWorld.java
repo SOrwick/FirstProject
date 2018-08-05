f9cb68145dc25eddf100082baf63a8c797c75c5f
SOrwick <42100402+SOrwick@users.noreply.github.com>
2018-08-05 10:50:09 -0400

    first add

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int y = 2;
+        int x = 13;
+        int answer =  x+y;
+
+        System.out.println(answer + " is the answer");
+    }
+}
